c8ab0101f53db8478d8d09c9f77e2c2d5f6be6e8
Otto Szarata <szystems@hotmail.com>
2023-09-12 16:01:47 -0600

    ajuste en Payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\inscription;
+use App\Models\Inscription;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
