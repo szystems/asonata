111bd3a78d331f3c64d3254ae9fa8630e1ce80ea
Otto Szarata <szystems@hotmail.com>
2024-01-05 16:56:27 -0600

    contar tallas en ciclos

--- resources/views/admin/class/indexcycles.blade.php
+++ resources/views/admin/class/indexcycles.blade.php
@@ -30,6 +30,8 @@
                                                 <div class="card" style="width: 20rem;">
 
                                                     <div class="card-body" align="center">
+                                                        <a href="{{ url('index_classes/'.$cycle->id) }}" type="button" class="btn btn-info"><i class="material-icons">visibility</i></a>
+                                                        <hr class="horizontal dark my-3">
                                                         <a href="{{ url('index_classes/'.$cycle->id) }}"><h5 class="card-title"><u>{{ $cycle->name }}</u></h5></a>
                                                         <hr class="horizontal dark my-3">
                                                         @php
@@ -39,11 +41,72 @@
                                                             ->where('status',1)
                                                             ->where('cycle_id',$cycle->id)
                                                             ->get();
+
+                                                            $athlets=DB::table('inscriptions')
+                                                            ->where('cycle_id',$cycle->id)
+                                                            ->where('inscription_status', 1)
+                                                            ->get();
                                                         @endphp
                                                         <p><b><font color="limegreen">{{ $start_date }}</font> - <font color="red">{{ $end_date }}</font></b></p>
-                                                        <p>{{ __('Classes') }}: {{ $classes->count() }}</p>
+                                                        <p>
+                                                            <b>{{ __('Classes') }}:</b> {{ $classes->count() }} <br>
+                                                            <b>{{ __('Athletes') }}:</b> {{ $athlets->count() }} <br>
+                                                            @if ($athlets->count() != 0)
+                                                            <b><u>{{ __('Tallas') }}:</u></b> <br>
+                                                            @endif
+                                                            @php
+                                                                $talla = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+                                                                //21 = XL
+                                                                //20 = L
+                                                                //19 = M
+                                                                //18 = S
+                                                            @endphp
+                                                            @foreach ($athlets as $athlet)
+                                                                @php
+                                                                    $atleta = \App\Models\Atleta::find($athlet->atleta_id);
 
-                                                        <a href="{{ url('index_classes/'.$cycle->id) }}" type="button" class="btn btn-info"><i class="material-icons">visibility</i></a>
+                                                                    for ($i=0; $i <= 16 ; $i++) {
+                                                                        if($atleta->tshirt_size == $i)
+                                                                        {
+                                                                            $talla[$i]++;
+                                                                        }
+
+                                                                    }
+                                                                    if ($atleta->tshirt_size == "XL") {
+                                                                            $talla[20]++;
+                                                                    }
+                                                                    if ($atleta->tshirt_size == "L") {
+                                                                        $talla[19]++;
+                                                                    }
+
+                                                                    if ($atleta->tshirt_size == "M") {
+                                                                        $talla[18]++;
+                                                                    }
+                                                                    if ($atleta->tshirt_size == "S") {
+                                                                        $talla[17]++;
+                                                                    }
+                                                                @endphp
+                                                            @endforeach
+                                                            @if ($talla[17] != 0)
+                                                                <p class="text-xs text-secondary mb-0"><b>S:</b>&nbsp;&nbsp;{{$talla[17]}}</p>
+                                                            @endif
+                                                            @if ($talla[18] != 0)
+                                                                <p class="text-xs text-secondary mb-0"><b>M:</b>&nbsp;&nbsp;{{$talla[18]}}</p>
+                                                            @endif
+                                                            @if ($talla[19] != 0)
+                                                                <p class="text-xs text-secondary mb-0"><b>L:</b>&nbsp;&nbsp;{{$talla[19]}}</p>
+                                                            @endif
+                                                            @if ($talla[20] != 0)
+                                                                <p class="text-xs text-secondary mb-0"><b>XL:</b>&nbsp;&nbsp;{{$talla[20]}}</p>
+                                                            @endif
+                                                            @for ($i = 1; $i <= 16; $i++)
+                                                                @if ($talla[$i] != 0)
+                                                                    <p class="text-xs text-secondary mb-0"><b>{{$i}}:</b>&nbsp;&nbsp;{{$talla[$i]}}</p>
+                                                                @endif
+                                                            @endfor
+                                                        </p>
+
+
 
                                                     </div>
                                                 </div>
